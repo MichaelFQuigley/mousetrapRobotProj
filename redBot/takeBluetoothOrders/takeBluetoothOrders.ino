f4df46dd752368a73b5054e54b8c2f78a3dcef6b
unknown <czufelt@sloth.arbfund.com>
2015-07-15 15:27:06 -0600

    Updating code for RedBot so it will tell the brain if it runs into something bad like a mousetrap or a cliff

--- redBot/takeBluetoothOrders/takeBluetoothOrders.ino
+++ redBot/takeBluetoothOrders/takeBluetoothOrders.ino
@@ -1,70 +1,50 @@
-#define    L_CTRL1   2
-#define    L_CTRL2   4
-#define    L_PWM     5
+#include <RedBot.h>
 
-#define    R_CTRL1   7
-#define    R_CTRL2   8
-#define    R_PWM     6
+// Instantiate the motors.
+RedBotMotors motors;
+
+// Instantiate the sensors.
+RedBotSensor leftSensor = RedBotSensor(A3);
+RedBotSensor middleSensor = RedBotSensor(A2);
+RedBotSensor rightSensor = RedBotSensor(A7);
 
 String leftWheel = "0";
 char delimeter(',');
 String rightWheel = "0";
 char endOrders('$');
+
 int waitBetweenOrders = 1000; // milliseconds
+const int sensorThreshold = 600;
+bool obeyBlindly = true;
 
 void setup() {
   // Start serial port at 9600 bps
   Serial.begin(9600);
-  pinMode(L_CTRL1, OUTPUT);
-  pinMode(L_CTRL2, OUTPUT);
-  pinMode(L_PWM, OUTPUT);
-  pinMode(R_CTRL1, OUTPUT);
-  pinMode(R_CTRL2, OUTPUT);
-  pinMode(R_PWM, OUTPUT);
+  Serial.println("Setup complete");
 }
 
 void loop() {
-  // If there are updated orders from the brain, do them
+  // If we run into a mousetrap or line, stop and tell the brain
+  if (obeyBlindly && (leftSensor.read() < sensorThreshold || middleSensor.read() < sensorThreshold || rightSensor.read() < sensorThreshold)) {
+    motors.brake();
+    obeyBlindly = false;
+    Serial.println("YOU LIED TO ME!!!");
+    Serial.write("DANGER");
+  }
+  
+  // If there are orders from the brain, do them (most of the time)
   if (Serial.available() > 0) {
     leftWheel = Serial.readStringUntil(delimeter);
     rightWheel = Serial.readStringUntil(endOrders);
-    Serial.println("Left wheel: " + leftWheel + "    Right wheel: " + rightWheel);
-    leftMotor(leftWheel.toInt());
-    rightMotor(rightWheel.toInt());
-    delay(waitBetweenOrders);
-  }
-}
-
-void leftMotor(int motorPower)
-{
-  motorPower = constrain(motorPower, -255, 255);   // constrain motorPower to -255 to +255
-  if(motorPower >= 0)
-  {
-    digitalWrite(L_CTRL1, HIGH);
-    digitalWrite(L_CTRL2, LOW);
-    analogWrite(L_PWM, abs(motorPower));
-  }
-  else
-  {
-    digitalWrite(L_CTRL1, LOW);
-    digitalWrite(L_CTRL2, HIGH);
-    analogWrite(L_PWM, abs(motorPower));
-  }
-}
-
-void rightMotor(int motorPower)
-{
-  motorPower = constrain(motorPower, -255, 255);   // constrain motorPower to -255 to +255
-  if(motorPower <= 0)
-  {
-    digitalWrite(R_CTRL1, HIGH);
-    digitalWrite(R_CTRL2, LOW);
-    analogWrite(R_PWM, abs(motorPower));
-  }
-  else
-  {
-    digitalWrite(R_CTRL1, LOW);
-    digitalWrite(R_CTRL2, HIGH);
-    analogWrite(R_PWM, abs(motorPower));
+    if (obeyBlindly || (leftWheel.toInt() <= 0 && rightWheel.toInt() <= 0)) {
+      obeyBlindly = true;
+      Serial.println("Left wheel: " + leftWheel + "    Right wheel: " + rightWheel);
+      motors.leftDrive(leftWheel.toInt());
+      motors.rightDrive(rightWheel.toInt());
+      delay(waitBetweenOrders);
+    }
+    else {
+      Serial.println("I'm not listening to you . . . ");
+    }
   }
 }
