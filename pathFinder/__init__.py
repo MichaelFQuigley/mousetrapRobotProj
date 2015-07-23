4d8b68b437aa5a67516a2b6dc178fe9fc817baae
Bruce Armstrong <bruce@fortressofgeekdom.org>
2015-07-23 15:17:57 -0600

    Made pathFinder usable as a module

---
+++ pathFinder/__init__.py
@@ -0,0 +1,27 @@
+from pathFinderDijkstra import PathFinderDijkstra as Dijkstra
+from path_as import PathFinderAStar as AStar
+from path_bfs import PathFinderBFS as BFS
+
+types = {
+    'Dijkstra': Dijkstra,
+    'AStar': AStar,
+    'BFS': BFS}
+
+current = None
+solver = None
+
+def get_types():
+    return types.keys()
+
+def set_type(t):
+    global current, solver
+    if types.has_key(t):
+        solver = types[t]
+        current = t
+
+def find_path(grid, origin, dest):
+    global solver
+    inst = solver(grid)
+    return inst.get_path(origin, dest)
+
+set_type('AStar')
