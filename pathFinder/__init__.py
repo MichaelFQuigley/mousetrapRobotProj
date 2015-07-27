cda7ef2f4100c2a746dd0909ebb3b7a9318b9c63
Pedro Tejada <pt314@users.noreply.github.com>
2015-07-27 14:43:44 -0600

    Move weights computation to path finder module.

--- pathFinder/__init__.py
+++ pathFinder/__init__.py
@@ -1,3 +1,7 @@
+import time
+import numpy as np
+from Queue import Queue
+
 from pathFinderDijkstra import PathFinderDijkstra as Dijkstra
 from path_as import PathFinderAStar as AStar
 from path_bfs import PathFinderBFS as BFS
@@ -27,3 +31,66 @@ def find_path(grid, origin, dest, weights=None):
     return instance.get_path(origin, dest, weights)
 
 set_type('AStar')
+
+
+# Predetermined neighbor coordinates
+neighbor_cords_8 = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+neighbor_cords_4 = [(-1, 0), (0, -1), (0, 1), (1, 0)]
+
+def get_neighbors(row, col, height, width):
+        """
+        Get adjacent neighbors.
+        """
+        neighbors = []
+        for r, c in neighbor_cords_4:
+            n_row = row + r
+            n_col = col + c
+            if n_row >= 0 and n_row < height and n_col >= 0 and n_col < width:
+                neighbors.append((n_row, n_col))
+        return neighbors
+
+def compute_map_weights(the_map):
+    """
+    Add comment here...
+    """
+                        
+    h, w = the_map.shape
+    print "map size:", the_map.shape
+
+    dist = np.zeros(the_map.shape, dtype = np.uint) # distance from obstacles
+
+    # Start with all locations next to an obstacle
+    border = set([])
+    for row in range(h):
+        for col in range(w):
+            if the_map[row][col] == 0:
+                for n in get_neighbors(row, col, h, w):
+                    if the_map[n[0]][n[1]] != 0 and dist[n[0]][n[1]] == 0:
+                        dist[n[0]][n[1]] = 1
+                        border.add(n)
+    frontier = Queue()
+    for x in border:
+        frontier.put(x)
+
+    # Compute distance to obstacles for all free locations
+    max_dist = 0
+    while not frontier.empty():
+        row, col = frontier.get()
+        for n in get_neighbors(row, col, h, w):
+            if the_map[n[0]][n[1]] == 0 and dist[n[0]][n[1]] == 0:
+                d = dist[row][col] + 1
+                dist[n[0]][n[1]] = d
+                if d > max_dist:
+                    max_dist = d
+                frontier.put(n)
+
+    print("max distance: " + str(max_dist))
+
+    # Determine weights from distances (scaled to [0, 255])
+    weights = np.copy(the_map)
+    for row in range(h):
+        for col in range(w):
+            weights[row][col] = int( 255 - 255 * dist[row][col] // max_dist )
+
+    return weights
+
